718c8d24fea09bcd81a5594975ea719e4573dcf6
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-06-26 13:07:28 +0530

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,18 +1,29 @@
-const { Vonage } = require('@vonage/server-sdk')
-
+const { Vonage } = require('@vonage/server-sdk');
+const mongoose = require('mongoose');
 const Member = require('./models/members'); // Adjust the path as needed
 require('dotenv').config();
+
 const vonage = new Vonage({
     apiKey: process.env.API_KEY,
-    apiSecret:process.env.API_SECRET 
-  })
+    apiSecret: process.env.API_SECRET
+});
 
 const from = "Rishus Infotech";
 
+mongoose.connect(process.env.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 async function sendSMS(to, text) {
   try {
-    await vonage.sms.send({ to, from, text });
-    console.log('Message sent successfully to', to);
+    const to_number = "91" + to;
+    const response = await vonage.sms.send({ to: to_number, from, text });
+    if (response.messages[0]['status'] === '0') {
+      console.log('Message sent successfully to', to);
+    } else {
+      console.log('Failed to send message to', to, 'with error:', response.messages[0]['error-text']);
+    }
   } catch (error) {
     console.log('Error sending message to', to);
     console.error(error);
@@ -26,15 +37,15 @@ async function checkBirthdaysAndSendMessages() {
   try {
     const members = await Member.find();
 
-    members.forEach(member => {
+    for (const member of members) {
       const memberDob = new Date(member.dob);
       const memberMonthDay = `${memberDob.getMonth() + 1}-${memberDob.getDate()}`;
 
       if (memberMonthDay === todayMonthDay) {
         const text = `Happy Birthday, ${member.name}! Thank you for your generous donation.`;
-        sendSMS(member.number, text);
+        await sendSMS(member.number, text); // Ensure sendSMS is awaited
       }
-    });
+    }
   } catch (error) {
     console.error('Error fetching members:', error);
   }
