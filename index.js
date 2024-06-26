7b20d447fc8246dcd60b00ce16b84d53bb84398e
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-06-26 12:39:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cron = require('node-cron');
 const app = express();
-
-const checkBirthdaysAndSendMessages = require('./birthday_scheduler')
 // Middleware
 require('dotenv').config();
 app.use(cors());
@@ -19,13 +17,6 @@ mongoose.connect(process.env.mongoURI, { useNewUrlParser: true, useUnifiedTopolo
 // Routes
 app.use('/api/members', require('./route/members'));
 app.use('/api/custom-message', require('./route/customMessage'));
-// Schedule the task to run every day at 12:00 AM
-cron.schedule('0 0 * * *', () => {
-  console.log('Running the birthday check task at 12:00 AM');
-  checkBirthdaysAndSendMessages();
-});
-
-console.log('Birthday message scheduler is running...');
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
